7f2f5e17ab8cadb7d112829361379880350bcd81
Iwan Buetti <iwan.buetti@gmail.com>
2014-06-19 23:23:44 +0200

    bumped version

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name              "nginx-tlq"
 maintainer        "Iwan Buetti"
 maintainer_email  "iwan.buetti+999@gmail.com"
 description       "Installs the nginx web server from the nginx ppa"
-version           "0.0.5"
+version           "0.0.6"
 
 recipe "nginx-tlq", "nginx server"
 
